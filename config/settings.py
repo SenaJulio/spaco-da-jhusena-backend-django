0a857a8f0ec997366551286a83d612dbd09bfe25
JULIO SENA <gm.sena@hotmail.com>
2025-12-19 21:18:24 -0300

    atualizando

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 
 
+WHITENOISE_MANIFEST_STRICT = False
 
 
 # =========================
