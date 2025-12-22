5b0a2685536d230d8b9df3e25e3337b27a1e8c34
JULIO SENA <gm.sena@hotmail.com>
2025-12-22 19:38:22 -0300

    DEBUG

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-only")
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-
 # =========================
 # Hardening (produção)
 # =========================
